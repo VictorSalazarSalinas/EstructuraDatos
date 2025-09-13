f33a3fb54774b2ed2cd715e44ff271e748d47a00
victor <victorsalazarsalinas@gmail.com>
2025-09-12 20:50:41 -0600

    ejercios clase 12 sep

---
+++ src/com.mx_curso/unidad 1/matriz/historial.py
@@ -0,0 +1,28 @@
+#lista de operaciones
+
+lista=[0.39,
+	  0.33,
+	  0.3,
+	  0.74,
+	  0.71,
+	  0.95,
+	  0.98,
+	  0.2 
+	  		]
+
+#imprimir lista
+print(lista)
+	
+#agregar elemento a la  lista
+lista.append(.80)
+
+#imprimir lista nuev
+print(lista)
+#ultimo elemento lista
+print(lista[-1])
+
+#max elemento de la lista
+print(max(lista))
+
+
+
