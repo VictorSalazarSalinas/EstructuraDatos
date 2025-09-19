151d725df0ae9afc60c9c9faeeb175717c81b120
victor <victorsalazarsalinas@gmail.com>
2025-09-19 08:04:09 -0600

    19 sep 2

---
+++ src/com.mx_curso/unidad 1/matriz/numpy/creaccion_matriz_numpy.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+
+#creacion de una matriz 3X3
+matriz_A=np.array([
+					[1,2,3],
+					[4,5,6],
+					[7,8,9]
+	])
+
+
+print(matriz_A)
+
+
+print("##########################")
+
+
+matriz_B=np.zeros([2,3])
+
+print(matriz_B)
+
+
+
+
+
+
+
