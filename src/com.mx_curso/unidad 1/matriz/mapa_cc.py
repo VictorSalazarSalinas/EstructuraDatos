c8c3d44c210fb559a9a552272167c598c69b367c
victor <victorsalazarsalinas@gmail.com>
2025-10-10 08:13:27 -0600

    ejercicios 10 oct

---
+++ src/com.mx_curso/unidad 1/matriz/mapa_cc.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+
+#creacion de una matriz 3X3
+matriz_A=np.array([
+					[1,2,3],
+					[4,5,6],
+					[7,8,9]
+	])
+
+for fila in matriz_A:
+	for elemento in fila:
+		print(elemento,end="")
+	print("")
+
+print("deseas modificar una temperatuta")
+mod=int(input("1:si  2:no"))
+
+if mod==1:
+	fila=int(input("dame fila"))
+	columna=int(input("dame la columna"))
+	valor=int(input("dame la nueva temperatuta"))
+	matriz_A[fila,columna]=valor
+
+
+for fila in matriz_A:
+	for elemento in fila:
+		print(elemento,end="")
+	print("")
