25edefcb01cdd1037fa47dcd382af176e5f70e31
victor <victorsalazarsalinas@gmail.com>
2025-10-17 08:32:33 -0600

    final 2

--- src/com.mx_curso/examen/diagonal.py
+++ src/com.mx_curso/examen/diagonal.py
@@ -3,28 +3,36 @@ def diagonal(a):
 	sumd2=0
 	u=0
 	
-	
+	#diagonal 1
 	for x in a:
-		i=0		
+
+		i=0
+		#print(u)
 		for z in x:
-			if i==u:				
+			if i==u:
+				#print(u)
 				sumd1=sumd1+z
 			i=i+1
-		u=u+1	
-	u=2
+		u=u+1
+
+	
+	u=len(a-1)
 	#diagonal 2
 	for x in a:
-		i=0		
+		i=0
+		#print(u)
 		for z in x:
-			if i==u:				
+			if i==u:
+				#print(u)
 				sumd2=sumd2+z
 			i=i+1
 		u=u-1
 	
-	print(sumd1-sumd2)	
-
+	print(sumd1-sumd2)
+		
 a=[ [1,2,3],
 	[4,5,6],
 	[7,8,9]]
+
 
 diagonal(a)
